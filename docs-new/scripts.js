f3da46a56e57e25bc68b3b7b3b75863ca22d2574
Ioana Verebi <ioanaverebi@gmail.com>
2018-12-23 00:19:09 +0200

    Fix highlighting

--- docs-new/scripts.js
+++ docs-new/scripts.js
@@ -3,19 +3,25 @@ function loadHTML(url, id) {
   req.open('GET', url);
   req.send();
   req.onload = () => {
-    document.getElementById(id).innerHTML = req.responseText;
+    modalReq = new XMLHttpRequest();
+    modalReq.open('GET', './views/modal.html');
+    modalReq.send();
+    modalReq.onload = () => {
+      document.getElementById('view').innerHTML = req.responseText + modalReq.responseText;
+      registerBehaviour();
+    }
   };
 }
 
 // use #! to hash
 router = new Navigo(null, true, '#');
 router.on({
-  'components': () => { loadHTML('./views/components.html', 'view'); },
-  'gallery': () => { loadHTML('./views/gallery.html', 'view'); },
-  'install': () => { loadHTML('./views/install.html', 'view'); }
+  'components': () => { loadHTML('./views/components.html'); },
+  'gallery': () => { loadHTML('./views/gallery.html'); },
+  'install': () => { loadHTML('./views/install.html'); }
 });
 
-router.on(() => { loadHTML('./views/home.html', 'view'); });
+router.on(() => { loadHTML('./views/home.html'); });
 
 router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
 
