6f2e8c3ecb7b3c1b9edd8f916dac355bfad6a99b
Tudor Girba <tudor@tudorgirba.com>
2019-01-13 12:08:07 +0100

    add a skeletong for design principles

--- docs/scripts.js
+++ docs/scripts.js
@@ -17,6 +17,7 @@ function loadHTML(url, isHomePage) {
 router = new Navigo(null, true, '#');
 router.on({
   'components': () => { loadHTML('./views/components.html'); },
+  'design': () => { loadHTML('./views/design.html'); },
   'moldable': () => { loadHTML('./views/moldable.html'); },
   'gallery': () => { loadHTML('./views/gallery.html'); },
   'install': () => { loadHTML('./views/install.html'); },
