a20af7ae5c7312f657ebd93ef54c5c486b445862
Ioana Verebi <ioanaverebi@gmail.com>
2019-08-08 02:16:18 +0200

    Add _site folder

---
+++ _site/assets/js/custom.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+  registerBehaviour();
+});
+
+function registerBehaviour(isHomePage) {
+  window.scrollTo(0,0);
+  handleMenuSelection();
+  addVersionNumbers();
+  addPreview();
+  buildCarousels();
+}
+
+function addVersionNumbers() {
+  var version = '';
+  $.get('https://dl.feenk.com/gt/GToolkitWin64-release', (data) => {
+    $("#win64").attr("href", "https://dl.feenk.com/gt/" + data);
+    version = data.replace('GToolkitWin64-', '').replace('.zip', '');
+    $(".gtversion").text(version);
+  }); 
+  $.get('https://dl.feenk.com/gt/GToolkitOSX64-release', (data) => {
+    $("#osx64").attr("href", "https://dl.feenk.com/gt/" + data);
+    version = data.replace('GToolkitOSX64-', '').replace('.zip', '');
+    $(".gtversion").text(version);
+  });
+  $.get('https://dl.feenk.com/gt/GToolkitLinux64-release', (data) => {
+    $("#linux64").attr("href", "https://dl.feenk.com/gt/" + data);
+    version = data.replace('GToolkitLinux64-', '').replace('.zip', '');
+    $(".gtversion").text(version);
+  }); 
+
+  $.get('https://dl.feenk.com/gt/.releasedateinseconds', (data) => { 
+    var d = new Date (data * 1000);
+    const month = d.toLocaleString('en-us', { month: 'long' });
+    var datestring = month + " " + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
+    $(".releasedate").text(datestring);
+  }); 
+
+}
+
+function detectOS() {
+  var userPlatform = this.platform.os.family;
+  var platforms = $('.download-button');
+  for (var i=0; i < platforms.length; i++) {
+    var platform = platforms[i].getAttribute('data-switcher-content');
+    if (platform.toLowerCase().startsWith(userPlatform.toLowerCase())) {
+      $(platforms[i]).addClass('btn-default download-active');
+    }
+    else {
+     $(platforms[i]).addClass('download-inactive'); 
+    }
+  }
+}
+
+
+function buildCarousels() {
+  $('.carousel').each(function() {
+    var carousel = $(this);
+    if (carousel.find('ol').length == 0) {
+      var ol = document.createElement('ol');
+      ol.className = 'carousel-indicators';
+
+      for (var i = 0; i < $(this).find('.carousel-item').length; i++) {
+        var li = document.createElement('li');
+        if (i == 0)
+          li.className = 'active';
+        li.setAttribute('data-target', '#'+carousel.attr('id'));
+        li.setAttribute('data-slide-to', i);
+        ol.append(li);
+      }
+      carousel.prepend(ol);
+
+      carousel.append(createControl('left', 'prev', carousel.attr('id')));
+      carousel.append(createControl('right', 'next', carousel.attr('id')));
+    }
+  });
+}
+
+function createControl(position, slideTo, carouselId) {
+  var a = document.createElement('a');
+  a.className = position + ' carousel-control carousel-control-'+slideTo;
+  a.setAttribute('data-target', '#' + carouselId);
+  a.setAttribute('role', 'button');
+  a.setAttribute('data-slide', slideTo);
+  var spanIcon = document.createElement('i');
+  spanIcon.className = "fas fa-fw fa-3x fa-chevron"+position;
+  spanIcon.setAttribute('aria-hidden', true);
+  a.append(spanIcon);
+  return a;
+}
+
+
+function addPreview() {
+  $('.thumbnail a, .carousel img, .carousel video').click(function(e) { 
+    e.preventDefault();
+    $("#expandedModal .modal-body").html($(e.target).clone());
+    $("#expandedModal .modal-footer p").text(e.target.nextElementSibling.innerText.trim());
+    $("#expandedModal").modal('show');
+  });
+}
+
+function handleMenuSelection() {
+  $('.nav li a').removeClass('active');
+
+  route = window.location.hash.substr(1).trim();
+  if (route == '') route = '/';
+
+  $('.nav li a[href="'+route+'"]').addClass('active');
+
+  $('.nav li a').on('click', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+}
---
+++ _site/docs-old/scripts.js
@@ -0,0 +1,32 @@
+function loadHTML(url, isHomePage) {
+  req = new XMLHttpRequest();
+  req.open('GET', url);
+  req.send();
+  req.onload = () => {
+    modalReq = new XMLHttpRequest();
+    modalReq.open('GET', './views/modal.html');
+    modalReq.send();
+    modalReq.onload = () => {
+      document.getElementById('view').innerHTML = req.responseText + modalReq.responseText;
+      registerBehaviour(isHomePage);
+    }
+  };
+}
+
+// use #! to hash
+router = new Navigo(null, true, '#');
+router.on({
+  'components': () => { loadHTML('./views/components.html'); },
+  'start': () => { loadHTML('./views/start.html'); },
+  'design': () => { loadHTML('./views/design.html'); },
+  'moldable': () => { loadHTML('./views/moldable.html'); },
+  'gallery': () => { loadHTML('./views/gallery.html'); },
+  'install': () => { loadHTML('./views/install.html'); },
+  'about': () => { loadHTML('./views/about.html'); }
+});
+
+router.on(() => { loadHTML('./views/home.html', true); });
+
+router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
+
+router.resolve();
