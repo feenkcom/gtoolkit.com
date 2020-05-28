a58e012a9384b4c7648f120b53f19ae502ded172
Ioana Verebi <ioanaverebi@gmail.com>
2020-05-28 15:50:34 +0300

    Platform detection

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,64 +1,75 @@
 window.onload = function registerBehaviour(isHomePage) {
   window.scrollTo(0, 0);
   handleMenuSelection();
+  detectOS();
   addVersionNumbers();
-}
+};
 
 function addVersionNumbers() {
   $.get(
     {
-      url: 'https://dl.feenk.com/gt/GlamorousToolkitWin64-release',
-      cache: false
+      url: "https://dl.feenk.com/gt/GlamorousToolkitWin64-release",
+      cache: false,
     },
     (data) => {
-      addVersionNumbersToButton('win64', 'GlamorousToolkitWin64-', data);
-    });
+      addVersionNumbersToButton("win64", "GlamorousToolkitWin64-", data);
+    }
+  );
   $.get(
     {
-      url: 'https://dl.feenk.com/gt/GlamorousToolkitOSX64-release',
-      cache: false
+      url: "https://dl.feenk.com/gt/GlamorousToolkitOSX64-release",
+      cache: false,
     },
     (data) => {
-      addVersionNumbersToButton('osx64', 'GlamorousToolkitOSX64-', data);
-    });
+      addVersionNumbersToButton("osx64", "GlamorousToolkitOSX64-", data);
+    }
+  );
   $.get(
     {
-      url: 'https://dl.feenk.com/gt/GlamorousToolkitLinux64-release',
-      cache: false
+      url: "https://dl.feenk.com/gt/GlamorousToolkitLinux64-release",
+      cache: false,
     },
     (data) => {
-      addVersionNumbersToButton('linux64', 'GlamorousToolkitLinux64-', data);
-    });
+      addVersionNumbersToButton("linux64", "GlamorousToolkitLinux64-", data);
+    }
+  );
 
   $.get(
     {
-      url: 'https://dl.feenk.com/gt/.releasedateinseconds',
-      cache: false
+      url: "https://dl.feenk.com/gt/.releasedateinseconds",
+      cache: false,
     },
     (data) => {
       var d = new Date(data * 1000);
-      const month = d.toLocaleString('en-us', { month: 'long' });
-      var datestring = month + " " + d.getDate() + " " + d.getHours() + ":" + ((d.getMinutes() < 10 ? '0' : '') + d.getMinutes());
+      const month = d.toLocaleString("en-us", { month: "long" });
+      var datestring =
+        month +
+        " " +
+        d.getDate() +
+        " " +
+        d.getHours() +
+        ":" +
+        ((d.getMinutes() < 10 ? "0" : "") + d.getMinutes());
       $(".releasedate").text(datestring);
-    });
+    }
+  );
 }
 
 function addVersionNumbersToButton(buttonId, downloadIdentifier, data) {
-  $('#' + buttonId).attr("href", "https://dl.feenk.com/gt/" + data);
-  var version = data.replace(downloadIdentifier, '').replace('.zip', '');
+  $("#" + buttonId).attr("href", "https://dl.feenk.com/gt/" + data);
+  var version = data.replace(downloadIdentifier, "").replace(".zip", "");
   $(".gtversion").text(version);
 }
 
 function detectOS() {
   var userPlatform = this.platform.os.family;
-  var platforms = $('.download-button');
+  var platforms = $(".download-button");
   for (var i = 0; i < platforms.length; i++) {
-    var platform = platforms[i].getAttribute('data-switcher-content');
+    var platform = platforms[i].getAttribute("data-switcher-content");
     if (platform.toLowerCase().startsWith(userPlatform.toLowerCase())) {
-      $(platforms[i]).addClass('btn-default download-active');
-    }
-    else {
-      $(platforms[i]).addClass('download-inactive');
+      $(platforms[i]).addClass("btn-default download-active");
+    } else {
+      $(platforms[i]).addClass("download-inactive");
     }
   }
 }
